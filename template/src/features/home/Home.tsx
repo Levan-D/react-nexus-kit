a190bf2f3b30c31b9e488bb4ab4ca06651bb314c
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-04-26 18:46:39 +0400

    Update Home.tsx

--- template/src/features/home/Home.tsx
+++ template/src/features/home/Home.tsx
@@ -6,32 +6,17 @@ export default function Home() {
   return (
     <div>
       <Helmet>
-        <title>Color Converter - CSS Mate</title>
-        <meta
-          name="description"
-          content="Easily convert between HSL, CMYK, HEX, RGB, and HSV color formats with our color converter app. Our app makes it easy to switch between different color formats and find the perfect color for your design project. Use our app to streamline your color conversion process and save time on your design projects."
-        />
+        <title>React Nexus Kit</title>
+        <meta name="description" content="Your description here" />
 
-        <meta
-          name="keywords"
-          content="color converter, HSL to CMYK, HEX to RGB, color formats, web design, graphic design, color matching, color selection, color conversion, color theory, color models, color space, color picker, color palette, color combinations"
-        />
+        <meta name="keywords" content="Your keywords here" />
 
-        <meta property="og:title" content="Color Converter" />
-        <meta
-          property="og:description"
-          content="Easily convert between HSL, CMYK, HEX, RGB, and HSV color formats with our color converter app. Our app makes it easy to switch between different color formats and find the perfect color for your design project. Use our app to streamline your color conversion process and save time on your design projects."
-        />
-        <meta
-          property="og:image"
-          content="https://cssmate.online/openGraphTags/colorConverter.png"
-        />
+        <meta property="og:title" content="Title for open graph" />
+        <meta property="og:description" content="Description for open graph" />
+        <meta property="og:image" content="Absolute link for your open graph image" />
 
         <meta property="og:type" content="website" />
-        <meta
-          property="og:url"
-          content="https://cssmate.online/converters/color-converter"
-        />
+        <meta property="og:url" content="absolute url of this page for your open graph" />
       </Helmet>
       <div>zis is ze content</div>
     </div>
