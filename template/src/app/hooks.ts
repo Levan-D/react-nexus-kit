c9f5cd0fbb8c5ca654437dae38eefc20f9295ed9
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-04-27 15:13:24 +0400

    Added basic comments and  explanations

--- template/src/app/hooks.ts
+++ template/src/app/hooks.ts
@@ -1,8 +1,16 @@
 /** @format */
 
-import { useDispatch, useSelector } from "react-redux";
-import type { TypedUseSelectorHook } from "react-redux";
-import type { RootState, AppDispatch } from "./store";
+import { useDispatch, useSelector } from "react-redux"
+import type { TypedUseSelectorHook } from "react-redux"
+import type { RootState, AppDispatch } from "./store"
 
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+/**
+ * Use pre-typed useDispatch and useSelector hooks for convenience and better type handling.
+ * - useSelector: No need to type (state: RootState) every time.
+ * - useDispatch: Handles thunks and middleware with the customized AppDispatch type.
+ * Define these hooks in a separate file (e.g., app/hooks.ts) to avoid circular import dependency issues.
+ */
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
